fa74d932344cf9dbf1596d8b6d7d444d25b9b3b1
BetaGo <BetaGo@users.noreply.github.com>
2020-08-23 12:54:11 +0000

    deploy: 6d8a4f45bda7c1d1cc32f640d71be0d5e6fa985e

---
+++ precache-manifest.e514f65fb78a7acb0e1817480ba5ca1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475aaaed319e66ed451175dfc6532541",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1868114b900b5971cf2",
+    "url": "/static/css/main.47b07509.chunk.css"
+  },
+  {
+    "revision": "e4dbdb145caa9af7e2cb",
+    "url": "/static/js/2.86a2fc19.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.86a2fc19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1868114b900b5971cf2",
+    "url": "/static/js/main.bbf6421d.chunk.js"
+  },
+  {
+    "revision": "986dff456c8f2bbef1ca",
+    "url": "/static/js/runtime-main.050ddfb8.js"
+  }
+]);
